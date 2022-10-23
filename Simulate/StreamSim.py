e5b273d0d88027bdb8056006758dd993eb395820
wbvguo <vinkwok@outlook.com>
2022-10-22 20:16:39 -0700

    clean

--- Simulate/StreamSim.py
+++ Simulate/StreamSim.py
@@ -2,44 +2,46 @@ import os
 import pickle
 import subprocess
 
+from array import array
 from typing import Dict, Union
 from Utils.UtilityFunctions import retrieve_iupac
 
+
 class StreamWGSIM:
     '''
-    stream the WGSIM output for bisulfite conversion
-    :param str sim_command: WGSIM commands for simulation
+    stream WGSIM output for bisulfite reads generation
+    :param str sim_cmd: WGSIM commands for simulation
     :rtype None
     '''
     
-    def __init__(self, sim_command: str = None):
-        self.sim_command = sim_command
+    def __init__(self, sim_cmd: str = None, pair_end: bool = True):
+        self.sim_cmd  = sim_cmd
+        self.pair_end = pair_end
     
     
     def __iter__(self):
-        sim = subprocess.Popen(self.sim_command, stdout=subprocess.PIPE, universal_newlines=True)
-        sim_iter = iter(sim.stdout.readline, b'')
+        wgsim = subprocess.Popen(self.sim_cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        sim_iter = iter(wgsim.stdout.readline, b'')
         
-        line, sim_iter = self.get_line(sim_iter) # line is None when EOF
+        line  = self.get_line(sim_iter) # line is None when EOF
         while line:
             if line == "Contig Variant Start":
                 # collect all the variant lines, after that sim_iter points to read lines
-                contig, variant_dict, sim_iter = self.collect_variants(sim_iter)
-                yield contig, variant_dict
+                variant_contig, variant_dict = self.collect_variants(sim_iter)
+                yield variant_contig, variant_dict
                 collect_reads_flag = True
             
             # collect read pairs
             while collect_reads_flag:
-                line, sim_iter = self.get_line(sim_iter)
-                
+                line = self.get_line(sim_iter)
                 if not line or line == "Contig Variant Start":
                     collect_reads_flag = False
                     break
                 
                 read1 = self.collect_read(sim_iter, line)
                 read2 = self.collect_read(sim_iter)
-                assert read1[0]["read_id"] == read2[0]["read_id"]
-                yield False, {0: read1, 1: read2}
+                assert read1["read_id"] == read2["read_id"]
+                yield False, [read1, read2]
                 
     
     def collect_variants(self, sim_iter):
@@ -48,10 +50,13 @@ class StreamWGSIM:
         while True:
             line = next(sim_iter).strip()
             if line == 'Contig Variant End':
-                return contig, variant_dict, sim_iter
-            
-            contig, variant_info = self.process_variant_line(line)
-            if variant_info:
+                if isinstance(variant_info, dict):
+                    return variant_info['chrom'], variant_dict
+                else:
+                    return variant_info, variant_dict
+       
+            variant_info = self.process_variant_line(line)
+            if isinstance(variant_info, dict):
                 assert variant_info['pos'] not in variant_dict
                 variant_dict[variant_info['pos']] = variant_info
     
@@ -59,11 +64,11 @@ class StreamWGSIM:
     def collect_read(self, sim_iter, line = None):
         if not line:
             line = next(sim_iter).strip()
-        info= self.process_read_name(line)
-        seq = next(sim_iter).strip()
-        cmt = next(sim_iter).strip()
-        qual= next(sim_iter).strip()
-        return [info, seq, cmt, qual]
+        read_dict= self.process_read_name(line)
+        read_dict['seq'] = next(sim_iter).strip()
+        next(sim_iter)
+        next(sim_iter)
+        return read_dict
     
 
     @staticmethod
@@ -72,11 +77,11 @@ class StreamWGSIM:
             line = next(sim_iter).strip()
         except StopIteration:
             print("End of output\n")
-            return None, sim_iter
+            return None
         else:
-            return line, sim_iter
+            return line
 
-
+        
     @staticmethod
     def process_variant_line(line: str) -> Dict:
         line_split = line.split('\t')
@@ -84,22 +89,23 @@ class StreamWGSIM:
         try:
             chrom, pos, ref, alt, heter_flag = line_split
         except ValueError:
-            return line_split[0], None
+            return line_split[0]
         else:
             heter = True if heter_flag == '+' else False
             indel = int(ref == '-') - int(alt == '-') # indel=1 for ref=='-', -1 for alt=='-', o.w. 0
             iupac = retrieve_iupac(alt) if indel == 0 else None
-            return chrom, dict(chrom=chrom, pos=int(pos), ref=ref, alt=alt, 
-                               heter=heter, indel=indel, iupac=iupac)
+            return dict(chrom=chrom, pos=int(pos), ref=ref, alt=alt, 
+                        heter=heter, indel=indel, iupac=iupac)
 
     @staticmethod
-    def process_read_name(line: str) -> Dict[str, Union[str, int]]:
+    def process_read_name(line: str) -> Dict[str, Union[str, int, array]]:
         read_info = line.split(':')
         chrom, start, end, insert_size, read_id, cigar, pair, c_base_info, g_base_info = read_info
         return dict(chrom=chrom.replace('@', ''), start=int(start), end=int(end),
                     insert_size=insert_size, read_id=read_id, cigar=cigar, pair=int(pair),
                     c_base_info=c_base_info, g_base_info=g_base_info)
-        
+
+
 
 class StreamOutput:
     '''
