028c7d4260ef955c4120145a37ffab6b91a8b327
wbvguo <vinkwok@outlook.com>
2022-10-27 22:39:33 -0700

    v1.0.2

--- Simulate/UtilityFunctions.py
+++ Simulate/UtilityFunctions.py
@@ -1,7 +1,34 @@
 from distutils.version import LooseVersion
 import os
+import io
 import sys
+import gzip
 import pysam
+from typing import List, Union
+
+
+class OpenCGmap:
+
+    def __init__(self, cgmap: str = None):
+        if cgmap.endswith('.gz'):
+            self.f = io.BufferedReader(gzip.open(cgmap, 'rb'))
+        else:
+            self.f = open(cgmap, 'r')
+
+    def __iter__(self):
+        with self.f as cg:
+            while True:
+                line = cg.readline()
+                if not line:
+                    break
+                yield self.process_line(line)
+
+    @staticmethod
+    def process_line(line: Union[str, bytes]) -> List[str]:
+        if isinstance(line, bytes):
+            return line.decode('utf-8').replace('\n', '').split('\t')
+        else:
+            return line.replace('\n', '').split('\t')
 
 
 def complement(sequence):
