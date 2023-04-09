b7e421cda9bfc71100692fcedefc3ddbac38de9f
wbvguo <vinkwok@outlook.com>
2023-04-09 00:27:18 -0700

    update

--- test/script/test_htsim_compile.sh
+++ test/script/test_htsim_compile.sh
@@ -1,7 +1,5 @@
-# methdb
-c++ -g -O3 -fpermissive   src/methdb.cpp -o methdb -Bstatic -lz  -I/home/wbguo/iproject/BSReadSim/HTSLIB/htslib/ -L/home/wbguo/iproject/BSReadSim/HTSLIB/ -lhts -Wl,-rpath /home/wbguo/iproject/BSReadSim/HTSLIB -lgsl -lgslcblas -Wall
-
+# rrcut
+c++ -g -O3 -fpermissive   src/rrcut.cpp src/haplo.cpp src/struct.cpp -o rrcut -Bstatic -lz  -I/home/wbguo/iproject/BSReadSim/HTSLIB/htslib/ -L/home/wbguo/iproject/BSReadSim/HTSLIB/ -lhts -Wl,-rpath /home/wbguo/iproject/BSReadSim/HTSLIB -lgsl -lgslcblas
 
 # hstim
-
-c++ -g -O3 -fpermissive   src/methdb.cpp src/htsim.cpp -o htsim -Bstatic -lz  -I/home/wbguo/iproject/BSReadSim/HTSLIB/htslib/ -L/home/wbguo/iproject/BSReadSim/HTSLIB/ -lhts -Wl,-rpath /home/wbguo/iproject/BSReadSim/HTSLIB -lgsl -lgslcblas -Wall
+c++ -g -O3 -fpermissive   src/htsim.cpp src/haplo.cpp src/methdb.cpp src/mode.cpp src/struct.cpp -o htsim -Bstatic -lz  -I/home/wbguo/iproject/BSReadSim/HTSLIB/htslib/ -L/home/wbguo/iproject/BSReadSim/HTSLIB/ -lhts -Wl,-rpath /home/wbguo/iproject/BSReadSim/HTSLIB -lgsl -lgslcblas
